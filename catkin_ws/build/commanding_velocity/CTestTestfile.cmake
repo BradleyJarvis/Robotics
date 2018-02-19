af2f48177d3ddc1d4d7e5cf8a427c1005463db57
Bradley Jarvis <bradley.jarvis@arm.com>
2018-02-19 15:40:48 +0000

    remove unwanted files

--- catkin_ws/build/commanding_velocity/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/computing/catkin_ws/src/commanding_velocity
-# Build directory: /home/computing/catkin_ws/build/commanding_velocity
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
